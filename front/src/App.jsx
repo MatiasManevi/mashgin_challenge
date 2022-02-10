1bc7552e10a9716ea56db5fed8d797ac2a1d1c55
matiasmanevi <manevimatias@gmail.com>
2022-02-09 22:48:57 -0300

    Initial front files

---
+++ front/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+
+
+function App() {
+	return (
+		<Router>
+			<Switch>
+				<h1>App</h1>
+			</Switch>
+		</Router>
+	);
+}
+
+export default App;
