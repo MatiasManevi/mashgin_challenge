5e4d009f7e657a136b66cba419279305129bac55
matiasmanevi <manevimatias@gmail.com>
2022-02-15 12:14:04 -0300

    Move Header to items

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { Header } from 'components/organisms';
 import { Items } from 'components/pages';
 
 function App() {
 	return (
 		<Router>
-			<Header />
 			<Switch>
 				<Route path="/" exact component={Items} />
 			</Switch>
--- front/src/components/pages/Items/Items.jsx
+++ front/src/components/pages/Items/Items.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 
 import './Items.scss';
-import { Item } from 'components/organisms';
+import { Item, Header } from 'components/organisms';
 import { useItems } from './hooks';
 
 const Items = () => {
 	const { items } = useItems();
 
 	return (
-		<div className="items">
-			{items.map((item) => (
-				<Item key={item.id} item={item} />
-			))}
-		</div>
+		<>
+			<Header />
+			<div className="items">
+				{items.map((item) => (
+					<Item key={item.id} item={item} />
+				))}
+			</div>
+		</>
 	);
 };
 
