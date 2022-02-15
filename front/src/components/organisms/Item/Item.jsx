c2191db4af9cb04d91438bbb8447c4c9d2085662
matiasmanevi <manevimatias@gmail.com>
2022-02-15 12:06:48 -0300

    Cleanup

--- front/src/components/organisms/Item/Item.jsx
+++ front/src/components/organisms/Item/Item.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 
 import './Item.scss';
 import { Price, Button } from 'components/atoms';
