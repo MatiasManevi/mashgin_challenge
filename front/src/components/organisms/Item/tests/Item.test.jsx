3f47b3aa55b805838e80cf6714e1950a76c0e58e
matiasmanevi <manevimatias@gmail.com>
2022-03-05 16:10:30 -0300

    Deep snapshot

--- front/src/components/organisms/Item/tests/Item.test.jsx
+++ front/src/components/organisms/Item/tests/Item.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import Item from '../Item';
 import item from './item.fixtures';
@@ -7,7 +7,7 @@ import { ContainerProviders } from 'containers';
 
 describe('Item', () => {
 	it('should match snapshot', () => {
-		const wrapper = shallow(<ContainerProviders><Item item={item} /></ContainerProviders>);
+		const wrapper = mount(<ContainerProviders><Item item={item} /></ContainerProviders>);
 		expect(wrapper).toMatchSnapshot();
 	});
 });
